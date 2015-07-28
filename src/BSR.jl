cb4366ed23518f8ff51abf74885f21e84fa944a1
Jake Snell <jsnell@cs.toronto.edu>
2015-07-28 19:16:08 -0400

    add gpb_owt_ucm_strength

--- src/BSR.jl
+++ src/BSR.jl
@@ -4,7 +4,8 @@ using MATLAB
 
 export
     gpb,
-    gpb_owt_ucm
+    gpb_owt_ucm,
+    gpb_owt_ucm_strength
 
 include("matutil.jl")
 include("bsrfun.jl")
--- src/bsrfun.jl
+++ src/bsrfun.jl
@@ -23,11 +23,59 @@ end
 ##
 ##############################################################################
 
-function gpb_owt_ucm{T<:Real}(img::Array{T}, scale::T=0.0)
+function gpb_owt_ucm_labels_ucm2{T<:Real}(img::Array{T}; scale::T=0.0)
     gpb_res = gpb(img)
     @mput gpb_res
     eval_string("ucm2 = contours2ucm(gpb_res, 'doubleSize');")
     eval_string("labels2 = bwlabel(ucm2 <= $scale);")
     eval_string("labels = labels2(2:2:end, 2:2:end);")
-    convert(Matrix{Int}, @mget labels)
+
+    labels = convert(Matrix{Int}, @mget labels)
+    ucm2 = @mget ucm2
+
+    labels, ucm2
+end
+
+function gpb_owt_ucm{T<:Real}(img::Array{T}; scale::T=0.0)
+    gpb_owt_ucm_labels_ucm2(img, scale=scale)[1]
+end
+
+function gpb_owt_ucm_strength{T<:Real}(img::Array{T}; scale::T=0.0)
+    # return label along with corresponding strengths between
+    # each region
+    labels, ucm2 = gpb_owt_ucm_labels_ucm2(img, scale=scale)
+
+    hstrength = ucm2[2:2:end, 3:2:end]
+    vstrength = ucm2[3:2:end, 2:2:end]
+
+    nsp = maximum(labels)
+    strengthdict = Dict{NTuple{2,Int},Float64}()
+    nrows, ncols = size(labels)
+    for j = 1:ncols
+        for i = 1:nrows
+            # left to right
+            if j <= ncols - 1
+                left = labels[i, j]
+                right = labels[i, j+1]
+                if left != right
+                    strengthdict[(left, right)] = 
+                    strengthdict[(right, left)] = 
+                        hstrength[i, j]
+                end
+            end
+            
+            # top to bottom
+            if i <= nrows - 1
+                top = labels[i, j]
+                bottom = labels[i+1, j]
+                if top != bottom
+                    strengthdict[(top, bottom)] =
+                    strengthdict[(bottom, top)] =
+                        vstrength[i, j]
+                end
+            end
+        end
+    end
+
+    labels, strengthdict
 end
